30948e01b481580fa68e325f987f52ffc10ba20b
24348-sudo <24348@burnside.school.nz>
2026-05-04 14:00:26 +1200

    connecteed the database, created cursos

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('hello world')
+import sqlite3
+
+db = sqlite3.connect('xxbestfoods.db')
+cursor = db.cursor()
+sql = 'SELECT * FROM best_foods'
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+db.close() 
